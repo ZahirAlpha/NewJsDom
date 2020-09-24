cb07f5863ee420df9468b9afb34b58b89b31acbf
Zahir Rafiq <cresarts@hotmail.com>
2020-09-24 12:22:54 +0100

    Event Bubbling & Delegation

--- app.js
+++ app.js
@@ -364,13 +364,13 @@
 
 // Keyboard & Input events
 
-const form = document.querySelector('form');
-const taskInput = document.getElementById('task');
-const heading = document.querySelector('h5');
-const select = document.querySelector('select');
+// const form = document.querySelector('form');
+// const taskInput = document.getElementById('task');
+// const heading = document.querySelector('h5');
+// const select = document.querySelector('select');
 
 // Clear input
-taskInput.value = '';
+// taskInput.value = '';
 
 // form.addEventListener('submit', runEvent);
 
@@ -399,18 +399,61 @@ taskInput.value = '';
 // taskInput.addEventListener('input', runEvent);
 
 // change
-select.addEventListener('change', runEvent);
+// select.addEventListener('change', runEvent);
 
 
-function runEvent(e) {
-  console.log(`EVENT TYPE: ${e.type}`);
+// function runEvent(e) {
+//   console.log(`EVENT TYPE: ${e.type}`);
 
-  console.log(e.target.value);
+//   console.log(e.target.value);
 
-  // heading.innerText = e.target.value;
+//   // heading.innerText = e.target.value;
 
-  // Get input value
-  // console.log(taskInput.value)
+//   // Get input value
+//   // console.log(taskInput.value)
 
-  // e.preventDefault();
+//   // e.preventDefault();
+// }
+
+// Event Bubbling & Delegation
+
+// EVENT BUBBLING
+
+// document.querySelector('.card-title').addEventListener('click',
+//   function () {
+//     console.log('card-title');
+//   });
+
+// document.querySelector('.card-content').addEventListener('click',
+//   function () {
+//     console.log('card content');
+//   });
+
+// document.querySelector('.card').addEventListener('click',
+//   function () {
+//     console.log('card');
+//   });
+
+// document.querySelector('.col').addEventListener('click',
+//   function () {
+//     console.log('col');
+//   });
+
+// EVENT DELEGATION 
+
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', deleteItem);
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+  // if (e.target.parentElement.className === 'delete-item secondary-content') {
+  //   console.log('delete item');
+  // }
+
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    console.log('delete item');
+    e.target.parentElement.parentElement.remove();
+  }
 }
